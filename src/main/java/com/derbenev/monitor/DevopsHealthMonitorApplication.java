64d3eaabb7bd4aee8fb7ddb0516c7d33473550a7
Maks Derbenov <maxdcloudops@gmail.com>
2026-06-14 19:53:30 -0400

    feat: initial project setup with Spring Boot 4.1.0

---
+++ src/main/java/com/derbenev/monitor/DevopsHealthMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.derbenev.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsHealthMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsHealthMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/derbenev/monitor/DevopsHealthMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derbenev.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsHealthMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
